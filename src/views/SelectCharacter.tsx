14c376ee737caa2e2f0c8024fcfcd7a224f11719
unknown <samuel871211>
2021-09-10 10:27:09 +0800

    selectCharacter add some picture, grid

--- src/views/SelectCharacter.tsx
+++ src/views/SelectCharacter.tsx
@@ -1,7 +1,9 @@
 import guide from '../imgs/guide.png'
 import selectCharacterBackground from '../imgs/selectCharacterBackground.png'
-import Box from '@material-ui/core/Box'
+// import Box from '@material-ui/core/Box'
+import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core/styles'
+import { Box } from '@material-ui/core'
 // 目標 => 人數 => 性別 => 名字 => 顏色 => 職業
 
 
@@ -13,42 +15,44 @@ export default function SelectCharacter () {
         container: {
             width: '100%',
             height: '100%',
-            backgroundImage: `url(${selectCharacterBackground})`
-        },
-        topArea: {
-            height: '65%',
-            fontSize: '0px'
+            backgroundImage: `url(${selectCharacterBackground})`,
+            backgroundRepeat: 'no-repeat'
         },
         topLeftArea: {
-            display: 'inline-block',
-            width: '50%',
-            height: '100%'
+            height: '65%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            overflow: 'hidden'
         },
         topRightArea: {
-            display: 'inline-block',
-            width: '50%',
-            height: '100%'
+            height: '65%'
         },
         bottomArea: {
             height: '35%'
+        },
+        guideImg: {
+            clipPath: 'polygon(0% 0%, 33.3% 0%, 33.3% 33.3%, 0% 33.3%)',
+            width: '300%',
+            height: '300%'
         }
     }))
     const classes = styles()
     
     // template
     return (
-        <Box className={classes.container}>
-            <Box className={classes.topArea}>
-                <Box className={classes.topLeftArea}>
-                    <img></img>
+        <Grid container className={classes.container}>
+            <Grid item xs={6} className={classes.topLeftArea}>
+                <Box width="50vmin" height="50vmin">
+                    <img src={guide} className={classes.guideImg}></img>
                 </Box>
-                <Box className={classes.topRightArea}>
-                    
-                </Box>
-            </Box>
-            <Box className={classes.bottomArea}>
+            </Grid>
+            <Grid item xs={6} className={classes.topRightArea}>
 
-            </Box>
-        </Box>
+            </Grid>
+            <Grid item xs={12} className={classes.bottomArea}>
+
+            </Grid>
+        </Grid>
     )
 }

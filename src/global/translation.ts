0aae0b9e1e4f50dcc41c4d81a538822155e1b8c9
shemira <samuel871211@gmail.com>
2022-03-17 14:59:54 +0800

    doc: 新增function t的jsdoc

--- src/global/translation.ts
+++ src/global/translation.ts
@@ -5,12 +5,20 @@ import { LangTypes } from '.'
 import texts from './texts'
 const defaultLang = 'jp'
 export default function useTranslation (lang: LangTypes) {
+    /**
+     * @deprecated
+     */
     function switchLang (newLang: LangTypes): void { lang = newLang }
+    /**
+     * t stands for translate
+     * @param key original text to be translated
+     * @returns translated text if exist, else return original text
+     */
     function t (key: string): string {
         if (lang === defaultLang) return key
         const textObj = texts.get(key)
         if (!textObj) return key
         return textObj[lang]
     }
-    return { t, switchLang }
+    return { t }
 }
